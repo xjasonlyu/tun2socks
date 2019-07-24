107246a07554f84997ee748b83489567d1ece071
Jason <xjasonlyu@gmail.com>
2019-07-24 13:01:23 +0800

    resort stats output order

--- common/stats/session/session.go
+++ common/stats/session/session.go
@@ -55,6 +55,9 @@ func (s *simpleSessionStater) Start() error {
 		tablePrint := func(w io.Writer, sessions []stats.Session) {
 			_, _ = fmt.Fprintf(w, "<table style=\"border=4px solid\">")
 			_, _ = fmt.Fprintf(w, "<tr><td>Process Name</td><td>Network</td><td>Duration</td><td>Local Addr</td><td>Remote Addr</td><td>Upload Bytes</td><td>Download Bytes</td></tr>")
+			sort.Slice(sessions, func(i, j int) bool {
+				return sessions[i].SessionStart.Before(sessions[j].SessionStart)
+			})
 			for _, sess := range sessions {
 				_, _ = fmt.Fprintf(w, "<tr><td>%v</td><td>%v</td><td>%v</td><td>%v</td><td>%v</td><td>%v</td><td>%v</td></tr>",
 					sess.ProcessName,
