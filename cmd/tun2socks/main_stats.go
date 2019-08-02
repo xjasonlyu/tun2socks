4419d28652acd40606bb8089b9d6aa3bf0884cf0
Jason <xjasonlyu@gmail.com>
2019-08-02 20:24:37 +0800

    fix a bug

--- cmd/tun2socks/main_stats.go
+++ cmd/tun2socks/main_stats.go
@@ -12,11 +12,12 @@ func init() {
 	args.Stats = flag.Bool("stats", false, "Enable statistics")
 	args.StatsAddr = flag.String("statsAddr", "localhost:6001", "listen address of stats, open in your browser to view statistics")
 
-	session.StatsAddr = *args.StatsAddr
-	session.StatsVersion = version
-
 	addPostFlagsInitFn(func() {
 		if *args.Stats {
+			// stats variables
+			session.StatsAddr = *args.StatsAddr
+			session.StatsVersion = version
+
 			sessionStater = session.NewSimpleSessionStater()
 			sessionStater.Start()
 		} else {
