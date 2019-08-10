13ae4ee221526a8f3664f808e57306acd0e9e782
Jason <xjasonlyu@gmail.com>
2019-08-10 22:09:41 +0800

    track Conn struct

--- common/stats/stats.go
+++ common/stats/stats.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"net"
 	"sync/atomic"
 	"time"
 )
@@ -30,4 +31,60 @@ func (s *Session) AddUploadBytes(n int64) {
 
 func (s *Session) AddDownloadBytes(n int64) {
 	atomic.AddInt64(&s.DownloadBytes, n)
+}
+
+type SessionConn struct {
+	net.Conn
+	*Session
+}
+
+func NewSessionConn(conn net.Conn, session *Session) net.Conn {
+	return &SessionConn{
+		Conn:    conn,
+		Session: session,
+	}
+}
+
+func (c *SessionConn) Read(b []byte) (n int, err error) {
+	n, err = c.Conn.Read(b)
+	if n > 0 {
+		atomic.AddInt64(&c.DownloadBytes, int64(n))
+	}
+	return
+}
+
+func (c *SessionConn) Write(b []byte) (n int, err error) {
+	n, err = c.Conn.Write(b)
+	if n > 0 {
+		atomic.AddInt64(&c.UploadBytes, int64(n))
+	}
+	return
+}
+
+type SessionPacketConn struct {
+	net.PacketConn
+	*Session
+}
+
+func NewSessionPacketConn(conn net.PacketConn, session *Session) net.PacketConn {
+	return &SessionPacketConn{
+		PacketConn: conn,
+		Session:    session,
+	}
+}
+
+func (c *SessionPacketConn) ReadFrom(b []byte) (n int, addr net.Addr, err error) {
+	n, addr, err = c.PacketConn.ReadFrom(b)
+	if n > 0 {
+		atomic.AddInt64(&c.DownloadBytes, int64(n))
+	}
+	return
+}
+
+func (c *SessionPacketConn) WriteTo(b []byte, addr net.Addr) (n int, err error) {
+	n, err = c.PacketConn.WriteTo(b, addr)
+	if n > 0 {
+		atomic.AddInt64(&c.UploadBytes, int64(n))
+	}
+	return
 }
