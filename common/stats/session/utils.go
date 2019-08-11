7d745984aa9143dc8fd5ba6f9dbd318a5af50503
Jason <xjasonlyu@gmail.com>
2019-08-11 11:53:10 +0800

    upgrade session utils

--- common/stats/session/utils.go
+++ common/stats/session/utils.go
@@ -6,10 +6,18 @@ import (
 	"time"
 )
 
-var startTime = time.Now()
+var startTime time.Time
+
+func init() {
+	startTime = time.Now()
+}
+
+func date(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
 
 func now() string {
-	return time.Now().Format("2006-01-02 15:04:05")
+	return date(time.Now())
 }
 
 func uptime() string {
