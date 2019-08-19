9d7433260ea78895002593de7cda8e53a1c90399
Jason <xjasonlyu@gmail.com>
2019-08-19 14:41:35 +0800

    Update pool_test.go

--- common/fakeip/pool_test.go
+++ common/fakeip/pool_test.go
@@ -9,21 +9,21 @@ import (
 
 func TestPool_Basic(t *testing.T) {
 	_, ipnet, _ := net.ParseCIDR("192.168.0.1/29")
-	pool, _ := New(ipnet, 10)
+	pool, _ := New(ipnet, 10, 10)
 
 	first := pool.Lookup("foo.com")
 	last := pool.Lookup("bar.com")
 	bar, exist := pool.LookBack(last)
 
-	assert.True(t, first.Equal(net.IP{192, 168, 0, 2}))
-	assert.True(t, last.Equal(net.IP{192, 168, 0, 3}))
+	assert.True(t, first.Equal(net.IP{192, 168, 0, 10}))
+	assert.True(t, last.Equal(net.IP{192, 168, 0, 11}))
 	assert.True(t, exist)
 	assert.Equal(t, bar, "bar.com")
 }
 
 func TestPool_Cycle(t *testing.T) {
 	_, ipnet, _ := net.ParseCIDR("192.168.0.1/30")
-	pool, _ := New(ipnet, 10)
+	pool, _ := New(ipnet, 2, 10)
 
 	first := pool.Lookup("foo.com")
 	same := pool.Lookup("baz.com")
@@ -33,7 +33,7 @@ func TestPool_Cycle(t *testing.T) {
 
 func TestPool_MaxCacheSize(t *testing.T) {
 	_, ipnet, _ := net.ParseCIDR("192.168.0.1/24")
-	pool, _ := New(ipnet, 2)
+	pool, _ := New(ipnet, 2, 2)
 
 	first := pool.Lookup("foo.com")
 	pool.Lookup("bar.com")
@@ -45,7 +45,7 @@ func TestPool_MaxCacheSize(t *testing.T) {
 
 func TestPool_Error(t *testing.T) {
 	_, ipnet, _ := net.ParseCIDR("192.168.0.1/31")
-	_, err := New(ipnet, 10)
+	_, err := New(ipnet, 2, 10)
 
 	assert.Error(t, err)
 }
