e55da6f16c2a374dd925da29aebb11a7bb69e289
Jason <xjasonlyu@gmail.com>
2019-08-19 16:12:54 +0800

    remove a comma

--- cmd/main_fakedns.go
+++ cmd/main_fakedns.go
@@ -13,7 +13,7 @@ func init() {
 	args.EnableFakeDNS = flag.Bool("fakeDNS", false, "Enable fake DNS")
 	args.Hosts = flag.String("hosts", "", "DNS hosts mapping, e.g. 'example.com=1.1.1.1,example.net=2.2.2.2'")
 	args.HijackDNS = flag.String("hijackDNS", "", "Hijack the specific DNS query to get a fake ip, e.g. '*:53', '8.8.8.8:53,8.8.4.4:53'")
-	args.BackendDNS = flag.String("backendDNS", "8.8.8.8:53,8.8.4.4:53", "Backend DNS to resolve !TypeA or !ClassINET query. (must support tcp)")
+	args.BackendDNS = flag.String("backendDNS", "8.8.8.8:53,8.8.4.4:53", "Backend DNS to resolve !TypeA or !ClassINET query (must support tcp)")
 
 	registerInitFn(func() {
 		if *args.EnableFakeDNS {
