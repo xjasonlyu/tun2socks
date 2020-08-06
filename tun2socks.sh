bb6b2c93ba588e2e9e53820c326b5da085ea8a23
Jason <xjasonlyu@gmail.com>
2020-08-06 20:30:15 +0800

    Update tun2socks.sh

--- tun2socks.sh
+++ tun2socks.sh
@@ -3,7 +3,7 @@
 TUN="${TUN:-tun0}"
 ETH="${ETH:-eth0}"
 ETHGW="${ETHGW:-172.16.1.1}"
-TUNGW="${TUNGW:-240.0.0.1}"
+TUNGW="${TUNGW:-198.18.0.1}"
 PROXY="${PROXY:-172.16.1.2:1080}"
 MONITOR="${MONITOR:-0.0.0.0:80}"
 EXCLUDED="${EXCLUDED:-172.16.1.2/32}"
@@ -17,7 +17,7 @@ ip addr add $TUNGW/24 dev $TUN
 ip link set dev $TUN up
 
 # change default gateway
-ip route del default &> /dev/null
+ip route del default > /dev/null
 ip route add default via $TUNGW dev $TUN
 
 # add to ip route
@@ -25,9 +25,6 @@ for ip in $(echo $EXCLUDED | tr ',' '\n')
 do
     ip route add $ip via $ETHGW
 done
-
-# DNS settings
-echo "nameserver $TUNGW" >> /etc/resolv.conf
 
 exec /tun2socks -loglevel $LOGLEVEL \
     -tunName $TUN -proxyServer $PROXY \
