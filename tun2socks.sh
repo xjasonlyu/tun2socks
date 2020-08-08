dd573b067eb787997e85a2c0af85872152f4095e
Jason <xjasonlyu@gmail.com>
2020-08-08 18:16:19 +0800

    update Dockerfile ENV

--- tun2socks.sh
+++ tun2socks.sh
@@ -2,32 +2,32 @@
 
 TUN="${TUN:-tun0}"
 ETH="${ETH:-eth0}"
-ETHADDR="${ETHADDR:-172.16.1.1}"
-TUNADDR="${TUNADDR:-198.18.0.1}"
-TUNMASK="${TUNMASK:-255.254.0.0}"
+ETH_ADDR="${ETH_ADDR:-172.16.1.1}"
+TUN_ADDR="${TUN_ADDR:-198.18.0.1}"
+TUN_MASK="${TUN_MASK:-255.254.0.0}"
 PROXY="${PROXY:-172.16.1.2:1080}"
 LOGLEVEL="${LOGLEVEL:-warning}"
 EXCLUDED="${EXCLUDED:-172.16.1.2/32}"
 
 MONITOR="${MONITOR:-1}"
-MONITORADDR="${MONITORADDR:-0.0.0.0:80}"
+MONITOR_ADDR="${MONITOR_ADDR:-0.0.0.0:80}"
 FAKEDNS="${FAKEDNS:-1}"
-BACKENDDNS="${BACKENDDNS:-8.8.8.8:53}"
+BACKEND_DNS="${BACKEND_DNS:-8.8.8.8:53}"
 HOSTS="${HOSTS:-localhost=127.0.0.1}"
 
 # create tun device
 ip tuntap add mode tun dev "$TUN"
-ip addr add "$TUNADDR"/"$TUNMASK" dev "$TUN"
+ip addr add "$TUN_ADDR"/"$TUN_MASK" dev "$TUN"
 ip link set dev "$TUN" up
 
 # change default gateway
 ip route del default > /dev/null
-ip route add default via "$TUNADDR" dev "$TUN"
+ip route add default via "$TUN_ADDR" dev "$TUN"
 
 # add to ip route
 for ip in $(echo "$EXCLUDED" | tr ',' '\n')
 do
-    ip route add "$ip" via "$ETHADDR"
+    ip route add "$ip" via "$ETH_ADDR"
 done
 
 if [ -n "$EXTRACMD" ]; then
@@ -35,11 +35,11 @@ if [ -n "$EXTRACMD" ]; then
 fi
 
 if [ "$MONITOR" -ne 0 ]; then
-    ARGS="-monitor -monitorAddr $MONITORADDR"
+    ARGS="-monitor -monitorAddr $MONITOR_ADDR"
 fi
 
 if [ "$FAKEDNS" -ne 0 ]; then
-    ARGS="$ARGS -fakeDNS -hosts $HOSTS -backendDNS $BACKENDDNS"
+    ARGS="$ARGS -fakeDNS -hosts $HOSTS -backendDNS $BACKEND_DNS"
 fi
 
 eval exec /tun2socks -loglevel "$LOGLEVEL" \
