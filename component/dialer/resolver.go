7e6a9b6a0985482825fd199b92b0acf99fe150d2
xjasonlyu <xjasonlyu@gmail.com>
2021-02-11 16:04:20 +0800

    Chore: add a comment to dialer

--- component/dialer/resolver.go
+++ component/dialer/resolver.go
@@ -3,6 +3,8 @@ package dialer
 import "net"
 
 func init() {
+	// We must use this DialContext to query DNS
+	// when using net default resolver.
 	net.DefaultResolver.PreferGo = true
 	net.DefaultResolver.Dial = DialContext
 }
