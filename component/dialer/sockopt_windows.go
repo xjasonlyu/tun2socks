0f11bb6cc6429163977e3bb8bfe20fa61f5b0307
nange <cctonange@gmail.com>
2022-11-26 22:52:32 +0800

    make style changes

--- component/dialer/sockopt_windows.go
+++ component/dialer/sockopt_windows.go
@@ -40,7 +40,7 @@ func setSocketOptions(network, address string, c syscall.RawConn, opts *Options)
 			case "tcp6", "udp6":
 				innerErr = bindSocketToInterface6(windows.Handle(fd), uint32(opts.InterfaceIndex))
 				if network == "udp6" && ip == nil {
-					// the underlying IP net maybe IPv4 even if the 'network' param is 'udp6',
+					// The underlying IP net maybe IPv4 even if the `network` param is `udp6`,
 					// so we should bind socket to interface4 at the same time
 					innerErr = bindSocketToInterface4(windows.Handle(fd), uint32(opts.InterfaceIndex))
 				}
@@ -54,14 +54,13 @@ func setSocketOptions(network, address string, c syscall.RawConn, opts *Options)
 	return
 }
 
-func bindSocketToInterface4(handle windows.Handle, interfaceIndex uint32) error {
+func bindSocketToInterface4(handle windows.Handle, index uint32) error {
 	// For IPv4, this parameter must be an interface index in network byte order.
-	//
 	// Ref: https://learn.microsoft.com/en-us/windows/win32/winsock/ipproto-ip-socket-options
 	var bytes [4]byte
-	binary.BigEndian.PutUint32(bytes[:], interfaceIndex)
-	interfaceIndex = *(*uint32)(unsafe.Pointer(&bytes[0]))
-	return windows.SetsockoptInt(handle, windows.IPPROTO_IP, IP_UNICAST_IF, int(interfaceIndex))
+	binary.BigEndian.PutUint32(bytes[:], index)
+	index = *(*uint32)(unsafe.Pointer(&bytes[0]))
+	return windows.SetsockoptInt(handle, windows.IPPROTO_IP, IP_UNICAST_IF, int(index))
 }
 
 func bindSocketToInterface6(handle windows.Handle, interfaceIndex uint32) error {
