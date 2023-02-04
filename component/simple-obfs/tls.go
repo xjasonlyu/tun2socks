4a8bf64cb124e4a83c52d11af4722126db8ef6b9
xjasonlyu <xjasonlyu@gmail.com>
2023-02-04 14:26:41 +0800

    Improve: use crypto/rand

--- component/simple-obfs/tls.go
+++ component/simple-obfs/tls.go
@@ -2,18 +2,14 @@ package obfs
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 	"io"
-	"math/rand"
 	"net"
 	"time"
 
 	"github.com/xjasonlyu/tun2socks/v2/common/pool"
 )
-
-func init() {
-	rand.Seed(time.Now().Unix())
-}
 
 const (
 	chunkSize = 1 << 14 // 2 ** 14 == 16 * 1024
