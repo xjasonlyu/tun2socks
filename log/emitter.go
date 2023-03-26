22b15f6fabac2d9ee98a57a5e7af2ed168403d46
xjasonlyu <xjasonlyu@gmail.com>
2023-03-26 22:48:28 +0800

    Improve: redirect stack log

---
+++ log/emitter.go
@@ -0,0 +1,26 @@
+package log
+
+import (
+	"io"
+	"time"
+
+	glog "gvisor.dev/gvisor/pkg/log"
+)
+
+func init() {
+	EnableStackLog(true)
+}
+
+func EnableStackLog(v bool) {
+	if v {
+		glog.SetTarget(&emitter{}) // built-in logger
+	} else {
+		glog.SetTarget(&glog.Writer{Next: io.Discard})
+	}
+}
+
+type emitter struct{}
+
+func (e emitter) Emit(_ int, level glog.Level, _ time.Time, format string, args ...any) {
+	logf(Level(level)+2, "[STACK] "+format, args...)
+}
