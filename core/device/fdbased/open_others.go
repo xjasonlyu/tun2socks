8309fddef324b49fcc26029298f99829635d50d7
yusheng <admin@iloft.xyz>
2023-06-30 13:18:53 +0800

    Feature: fdbased open fd with offset (#272)

--- core/device/fdbased/open_others.go
+++ core/device/fdbased/open_others.go
@@ -10,10 +10,10 @@ import (
 	"github.com/xjasonlyu/tun2socks/v2/core/device/iobased"
 )
 
-func open(fd int, mtu uint32) (device.Device, error) {
+func open(fd int, mtu uint32, offset int) (device.Device, error) {
 	f := &FD{fd: fd, mtu: mtu}
 
-	ep, err := iobased.New(os.NewFile(uintptr(fd), f.Name()), mtu, 0)
+	ep, err := iobased.New(os.NewFile(uintptr(fd), f.Name()), mtu, offset)
 	if err != nil {
 		return nil, fmt.Errorf("create endpoint: %w", err)
 	}
