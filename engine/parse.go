03565b674850cf9bfe91246337b5c25f40d90388
hossinasaadi <hossin.asaadi@gmail.com>
2024-10-12 13:28:28 +0400

    fix lint

--- engine/parse.go
+++ engine/parse.go
@@ -68,7 +68,6 @@ func parseDevice(s string, mtu uint32) (device.Device, error) {
 }
 
 func parseFD(u *url.URL, mtu uint32) (device.Device, error) {
-
 	// fd offset in ios
 	// https://stackoverflow.com/questions/69260852/ios-network-extension-packet-parsing/69487795#69487795
 	if runtime.GOOS == "ios" {
