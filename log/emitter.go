d96e06fd6e5efbd24dbb68df61233d88ac2a1b79
Jason Lyu <xjasonlyu@gmail.com>
2026-01-08 11:29:15 -0500

    Fix(log): set log level for `glog` (#511)

--- log/emitter.go
+++ log/emitter.go
@@ -1,8 +1,6 @@
 package log
 
 import (
-	"runtime"
-	"strings"
 	"time"
 
 	glog "gvisor.dev/gvisor/pkg/log"
@@ -20,18 +18,13 @@ type emitter struct {
 
 func (e *emitter) setLogger(logger *SugaredLogger) {
 	e.logger = logger.WithOptions(pkgCallerSkip)
+	glog.SetLevel(1 - glog.Level(e.logger.Level()))
 }
 
 func (e *emitter) logf(level glog.Level, format string, args ...any) {
 	e.logger.Logf(1-Level(level), "[STACK] "+format, args...)
 }
 
-func (e *emitter) Emit(depth int, level glog.Level, _ time.Time, format string, args ...any) {
-	if _, file, line, ok := runtime.Caller(depth + 1); ok {
-		// Ignore: gvisor.dev/gvisor/pkg/tcpip/adapters/gonet/gonet.go:457
-		if line == 457 && strings.HasSuffix(file, "gonet/gonet.go") {
-			return
-		}
-	}
+func (e *emitter) Emit(_ int, level glog.Level, _ time.Time, format string, args ...any) {
 	e.logf(level, format, args...)
 }
