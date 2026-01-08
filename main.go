e613c3cd598eb807e70e0f200b34efba223f071d
Jason Lyu <xjasonlyu@gmail.com>
2026-01-08 12:12:09 -0500

    Feature: use pflag as drop-in replacement

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
+	flag "github.com/spf13/pflag"
 	"gopkg.in/yaml.v3"
 
 	_ "github.com/xjasonlyu/tun2socks/v2/dns"
@@ -23,14 +23,14 @@ var (
 )
 
 func init() {
-	flag.IntVar(&key.Mark, "fwmark", 0, "Set firewall MARK (Linux only)")
+	flag.IntVar(&key.Mark, "fwmark", 0, "Set firewall MARK (Linux/BSD)")
 	flag.IntVar(&key.MTU, "mtu", 0, "Set device maximum transmission unit (MTU)")
 	flag.DurationVar(&key.UDPTimeout, "udp-timeout", 0, "Set timeout for each UDP session")
-	flag.StringVar(&configFile, "config", "", "YAML format configuration file")
-	flag.StringVar(&key.Device, "device", "", "Use this device [driver://]name")
-	flag.StringVar(&key.Interface, "interface", "", "Use network INTERFACE (Linux/MacOS only)")
+	flag.StringVarP(&configFile, "config", "c", "", "YAML format configuration file")
+	flag.StringVarP(&key.Device, "device", "d", "", "Use this device [driver://]name")
+	flag.StringVarP(&key.Proxy, "proxy", "p", "", "Use this proxy [protocol://]host[:port]")
+	flag.StringVar(&key.Interface, "interface", "", "Use network INTERFACE (Linux/MacOS/Windows)")
 	flag.StringVar(&key.LogLevel, "loglevel", "info", "Log level [debug|info|warn|error|silent]")
-	flag.StringVar(&key.Proxy, "proxy", "", "Use this proxy [protocol://]host[:port]")
 	flag.StringVar(&key.RestAPI, "restapi", "", "HTTP statistic server listen address")
 	flag.StringVar(&key.TCPSendBufferSize, "tcp-sndbuf", "", "Set TCP send buffer size for netstack")
 	flag.StringVar(&key.TCPReceiveBufferSize, "tcp-rcvbuf", "", "Set TCP receive buffer size for netstack")
@@ -38,7 +38,7 @@ func init() {
 	flag.StringVar(&key.MulticastGroups, "multicast-groups", "", "Set multicast groups, separated by commas")
 	flag.StringVar(&key.TUNPreUp, "tun-pre-up", "", "Execute a command before TUN device setup")
 	flag.StringVar(&key.TUNPostUp, "tun-post-up", "", "Execute a command after TUN device setup")
-	flag.BoolVar(&versionFlag, "version", false, "Show version and then quit")
+	flag.BoolVarP(&versionFlag, "version", "v", false, "Show version and then quit")
 	flag.Parse()
 }
 
