41a1fc92609306239412bf88193431cd73d61ed8
Jason Lyu <xjasonlyu@gmail.com>
2026-01-22 18:28:50 -0500

    minor comments update

--- core/adapter/adapter.go
+++ core/adapter/adapter.go
@@ -6,19 +6,21 @@ import (
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
 )
 
-// TCPConn implements the net.Conn interface.
+// TCPConn represents a TCP connection that implements net.Conn
+// and exposes its stack.TransportEndpointID.
 type TCPConn interface {
 	net.Conn
 
-	// ID returns the transport endpoint id of TCPConn.
+	// ID returns the transport endpoint id.
 	ID() stack.TransportEndpointID
 }
 
-// UDPConn implements net.Conn and net.PacketConn.
+// UDPConn represents a UDP connection that implements both net.Conn
+// and net.PacketConn and exposes its stack.TransportEndpointID.
 type UDPConn interface {
 	net.Conn
 	net.PacketConn
 
-	// ID returns the transport endpoint id of UDPConn.
+	// ID returns the transport endpoint id.
 	ID() stack.TransportEndpointID
 }
