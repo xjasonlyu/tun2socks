b83466d0930668b1d8462eae710c625ed3857a03
Jason Lyu <xjasonlyu@gmail.com>
2026-02-07 16:18:31 -0500

    add comments

--- dialer/dialer.go
+++ dialer/dialer.go
@@ -9,29 +9,32 @@ import (
 	"go.uber.org/atomic"
 )
 
-// DefaultDialer is the default Dialer and is used by DialContext and ListenPacket.
+// DefaultDialer is the package-level default Dialer.
+// It is used by DialContext and ListenPacket.
 var DefaultDialer = &Dialer{}
 
-// RegisterSockOpt registers the given socket option to DefaultDialer.
+// RegisterSockOpt registers a socket option on the DefaultDialer.
 func RegisterSockOpt(opt SocketOption) {
 	DefaultDialer.RegisterSockOpt(opt)
 }
 
-// DialContext is a wrapper around DefaultDialer.DialContext.
+// DialContext dials using the DefaultDialer.
 func DialContext(ctx context.Context, network, address string) (net.Conn, error) {
 	return DefaultDialer.DialContext(ctx, network, address)
 }
 
-// ListenPacket is a wrapper around DefaultDialer.ListenPacket.
+// ListenPacket listens using the DefaultDialer.
 func ListenPacket(network, address string) (net.PacketConn, error) {
 	return DefaultDialer.ListenPacket(network, address)
 }
 
+// Dialer applies registered SocketOptions to all dials/listens.
 type Dialer struct {
 	optsMu     sync.Mutex
 	atomicOpts atomic.Value
 }
 
+// New creates a new Dialer with the given initial socket options.
 func New(opts ...SocketOption) *Dialer {
 	d := &Dialer{}
 	for _, opt := range opts {
@@ -40,6 +43,7 @@ func New(opts ...SocketOption) *Dialer {
 	return d
 }
 
+// RegisterSockOpt registers a socket option on the Dialer.
 func (d *Dialer) RegisterSockOpt(opt SocketOption) {
 	d.optsMu.Lock()
 	opts, _ := d.atomicOpts.Load().([]SocketOption)
@@ -66,14 +70,18 @@ func (d *Dialer) applySockOpts(network string, address string, c syscall.RawConn
 	return nil
 }
 
+// DialContext behaves like net.Dialer.DialContext, applying registered SocketOptions.
 func (d *Dialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
-	return (&net.Dialer{
+	nd := &net.Dialer{
 		Control: d.applySockOpts,
-	}).DialContext(ctx, network, address)
+	}
+	return nd.DialContext(ctx, network, address)
 }
 
+// ListenPacket behaves like net.ListenConfig.ListenPacket, applying registered SocketOptions.
 func (d *Dialer) ListenPacket(network, address string) (net.PacketConn, error) {
-	return (&net.ListenConfig{
+	lc := &net.ListenConfig{
 		Control: d.applySockOpts,
-	}).ListenPacket(context.Background(), network, address)
+	}
+	return lc.ListenPacket(context.Background(), network, address)
 }
